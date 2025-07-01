cb65260df363df141508dbb3d55b0761504a8376
Luigi1503 <montonettiluigi@icloud.com>
2025-07-01 16:42:10 +0200

    aggiornamento 2

--- app.py
+++ app.py
@@ -47,4 +47,5 @@ def mostra_conferme():
     return {'conferme': dati}
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
